642b5184d313938a33b960b58c465417a0f92c10
Shubham Borkar <78637025+Shubham-Borkar@users.noreply.github.com>
2024-12-01 19:21:40 +0530

    Add files via upload
    feat: 
    * Pagination and Sorting,
    * @JsonFilter Dynamic Filter (Alternative to ModelMapper),
    * EntityModel wrapping a domain object and adding links to it, 
    * i18n,
    * HATEOAS,
    * Api Versioning,
    * Validation with annotation
    * @ResponseStatus - Adding status code for Custom Exception

---
+++ A Extra feature gobal exception/product3/src/main/java/com/app/dto/ProductDto.java
@@ -0,0 +1,41 @@
+package com.app.dto;
+
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    @Size(min=3,message="name must be at least 3 character long")
+	private String name;
+	private float price;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ A Extra feature gobal exception/product3/src/main/java/com/app/pojo/Review.java
@@ -0,0 +1,73 @@
+package com.app.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="sss_review")
+public class Review {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty(access = Access.READ_ONLY)
+	private int review_Id;
+	@Column
+	private int ratings;
+	@Column
+	private String commment;
+	@ManyToOne
+	@JoinColumn(name="review_productid_fk")
+	@JsonIgnore
+	private Product product;
+
+	public Review() {
+		super();
+	}
+	
+	public Review(int review_Id, int ratings, String commment, Product product) {
+		super();
+		this.review_Id = review_Id;
+		this.ratings = ratings;
+		this.commment = commment;
+		this.product = product;
+	}
+
+	public int getReview_Id() {
+		return review_Id;
+	}
+	public void setReview_Id(int review_Id) {
+		this.review_Id = review_Id;
+	}
+	public int getRatings() {
+		return ratings;
+	}
+	public void setRatings(int ratings) {
+		this.ratings = ratings;
+	}
+	public String getCommment() {
+		return commment;
+	}
+	public void setCommment(String commment) {
+		this.commment = commment;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	@Override
+	public String toString() {
+		return "Review [review_Id=" + review_Id + ", ratings=" + ratings + ", commment=" + commment + "]";
+	}
+	
+}
